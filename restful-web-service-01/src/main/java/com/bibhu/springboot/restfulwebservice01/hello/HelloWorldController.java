69738485efd77457afc768d02b622bab083690be
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2023-08-10 22:49:05 +0530

    path variable integration

--- restful-web-service-01/src/main/java/com/bibhu/springboot/restfulwebservice01/hello/HelloWorldController.java
+++ restful-web-service-01/src/main/java/com/bibhu/springboot/restfulwebservice01/hello/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.bibhu.springboot.restfulwebservice01.hello;
 
 import com.bibhu.springboot.restfulwebservice01.hello.model.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World Message");
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean("Hello World Message " + name);
     }
 }
