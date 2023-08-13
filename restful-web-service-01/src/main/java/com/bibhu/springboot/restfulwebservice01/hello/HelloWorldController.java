708e6ab6c7df47479cd1f797fda9bcc3b31ba9e0
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2023-08-13 11:35:58 +0530

    implementing i18n support

--- restful-web-service-01/src/main/java/com/bibhu/springboot/restfulwebservice01/hello/HelloWorldController.java
+++ restful-web-service-01/src/main/java/com/bibhu/springboot/restfulwebservice01/hello/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.bibhu.springboot.restfulwebservice01.hello;
 
 import com.bibhu.springboot.restfulwebservice01.hello.model.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -21,5 +29,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean("Hello World Message " + name);
+    }
+
+    @GetMapping("/hello-world-i18n")
+    public String helloWorldI18() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
