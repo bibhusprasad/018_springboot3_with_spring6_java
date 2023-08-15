4d00aad0512e07e1335b3622dd1f45324bf998db
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2023-08-15 15:26:30 +0530

    versioning implementation

---
+++ restful-web-service-01/src/main/java/com/bibhu/springboot/restfulwebservice01/person/model/PersonV1.java
@@ -0,0 +1,17 @@
+package com.bibhu.springboot.restfulwebservice01.person.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class PersonV1 {
+
+    private String name;
+}
---
+++ restful-web-service-01/src/main/java/com/bibhu/springboot/restfulwebservice01/person/model/PersonV2.java
@@ -0,0 +1,17 @@
+package com.bibhu.springboot.restfulwebservice01.person.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class PersonV2 {
+
+    private Name name;
+}
