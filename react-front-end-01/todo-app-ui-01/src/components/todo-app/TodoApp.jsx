cc5f31b7bfc1a4bec4d09647c9e0a95bd2be61bb
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2023-08-22 12:27:21 +0530

    Adding header and footer

--- react-front-end-01/todo-app-ui-01/src/components/todo-app/TodoApp.jsx
+++ react-front-end-01/todo-app-ui-01/src/components/todo-app/TodoApp.jsx
@@ -7,13 +7,16 @@ export default function TodoApp() {
     return(
         <div className="TodoApp">
             <BrowserRouter>
+                <HeaderComponent/>
                 <Routes>
                     <Route path='/' element={<LoginComponent/>} />
                     <Route path='/login' element={<LoginComponent/>} />
                     <Route path='/welcome/:username' element={<WelcomeComponent/>} />
                     <Route path='/todos' element={<ListTodoComponent/>} />
+                    <Route path='/logout' element={<LogoutComponent/>} />
                     <Route path='*' element={<ErrorComponent/>} />
                 </Routes>
+                <FooterComponent/>
             </BrowserRouter>
         </div>
     )
@@ -171,6 +174,53 @@ function ListTodoComponent() {
                     }
                     </tbody>
                 </table>
+            </div>
+        </div>
+    )
+}
+
+
+function HeaderComponent() {
+    return(
+        <header className="border-bottom border-light border-5 mb-5 p-2 bg-light">
+            <div className="container">
+                <div className="row">
+                    <nav className="navbar navbar-expand-lg">
+                        <a className="navbar-brand ms-2 fs-2 fw-bold text-black">Todo App</a>
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav">
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/bibhu">Home</Link></li>
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                            </ul>
+                        </div>
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+
+    )
+}
+
+function FooterComponent() {
+    return(
+        <footer className = "FooterComponent">
+            <div className="container">
+                Your footer
+            </div>
+        </footer>
+    )
+}
+
+function LogoutComponent() {
+    return(
+        <div className = "LogoutComponent">
+            <h2>You are logged out!</h2>
+            <div>
+                Thank you for using our app.
             </div>
         </div>
     )
