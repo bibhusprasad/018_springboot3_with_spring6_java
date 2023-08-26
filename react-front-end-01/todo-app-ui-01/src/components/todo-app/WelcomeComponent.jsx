29801c2fdb1726e476cbfe1cbe911f402340d9a5
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2023-08-26 10:50:05 +0530

    React with Springboot: Enabling CORS Requests for Spring Boot REST API

--- react-front-end-01/todo-app-ui-01/src/components/todo-app/WelcomeComponent.jsx
+++ react-front-end-01/todo-app-ui-01/src/components/todo-app/WelcomeComponent.jsx
@@ -1,9 +1,12 @@
 import {Link, useParams} from "react-router-dom";
 import axios from "axios";
+import {useState} from "react";
 
 export default function WelcomeComponent() {
 
     const {username}= useParams()
+
+    const [message, setMessage] = useState(null)
 
     function callHelloWorldRestAPI() {
         console.log('called')
@@ -14,12 +17,28 @@ export default function WelcomeComponent() {
             .finally(() => console.log('cleanup code'))
     }
 
+    function callHelloWorldBeanRestAPI() {
+        console.log('called')
+        //axios to call rest API
+        axios.get('http://localhost:8080/hello-world-bean')
+            .then((response) => successBeanMessage(response))
+            .catch((error) => errorMessage(error))
+            .finally(() => console.log('cleanup code'))
+    }
+
     function successMessage(response) {
+        setMessage(response.data)
         console.log(response)
     }
 
     function errorMessage(error) {
+        setMessage(error.toString())
         console.log(error)
+    }
+
+    function successBeanMessage(response) {
+        setMessage(response.data.message)
+        console.log(response)
     }
 
     return(
@@ -33,6 +52,11 @@ export default function WelcomeComponent() {
                 <button className="btn btn-success m-3" onClick= {callHelloWorldRestAPI}>
                     Call Hello World Rest API</button>
             </div>
+            <div>
+                <button className="btn btn-success m-3" onClick= {callHelloWorldBeanRestAPI}>
+                    Call Hello World Bean Rest API</button>
+            </div>
+            <div className="text-info">{message}</div>
         </div>
     )
 }
