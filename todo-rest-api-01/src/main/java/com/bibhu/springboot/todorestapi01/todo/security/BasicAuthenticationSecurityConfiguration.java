5a8771702e1f9868d3f343bde7bcb3650a7aed75
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2023-08-29 20:21:40 +0530

    Spring Security : Basic authentication with username and password present in properties file with stateless rest api and disabling csrf

--- todo-rest-api-01/src/main/java/com/bibhu/springboot/todorestapi01/todo/security/BasicAuthenticationSecurityConfiguration.java
+++ todo-rest-api-01/src/main/java/com/bibhu/springboot/todorestapi01/todo/security/BasicAuthenticationSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,6 +19,12 @@ public class BasicAuthenticationSecurityConfiguration {
         );
         //Basic authentication with username and password present in properties file
         httpSecurity.httpBasic(Customizer.withDefaults());
+        //stateless rest api
+        httpSecurity.sessionManagement(
+            session -> session.sessionCreationPolicy(
+                    SessionCreationPolicy.STATELESS));
+        //disabling csrf
+        httpSecurity.csrf(csrf -> csrf.disable());
         return httpSecurity.build();
     }
 }
